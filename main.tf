b24aee97c55d6c0bcb2111c0f37bde3de9482035
Schalkjb <93654433+Schalkjb@users.noreply.github.com>
2024-02-21 11:07:47 +0200

    Fixed new error

--- main.tf
+++ main.tf
@@ -44,6 +44,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   cidr_blocks       = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
+}
 
   resource "aws_security_group_rule" "blog_https_in" {
   type              = "ingress"
@@ -53,6 +54,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   cidr_blocks       = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
+  }
 
   resource "aws_security_group_rule" "blog_everything_out" {
   type              = "egress"
